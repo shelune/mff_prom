4ec56166ea374dc901f74f5a1ae7d3e3dd4a7ef6
Your Name <you@example.com>
2016-05-04 01:07:33 +0300

    dynamic content to the performer -> details section, footer done as well

--- page-performers.php
+++ page-performers.php
@@ -7,6 +7,8 @@ $performer_overview_title = get_field('performer_overview_title');
 $performer_overview_desc = get_field('performer_overview_desc');
 $performer_overview_image = get_field('performer_overview_image');
 
+$performer_details_title = get_field('performer_details_title');
+
 get_header(); ?>
 
 	<section class="featured" data-type="background" data-speed="3">
@@ -18,7 +20,7 @@ get_header(); ?>
 			<div class="row">
 				<h1 class="text-center"><?php echo $performer_overview_title; ?></h1>
 				<div class="col-sm-4 overview-content">
-					<p><?php echo $performer_overview_desc ?></p>
+					<p><?php echo $performer_overview_desc; ?></p>
 				</div>
 				<div class="col-sm-8 overview-image">
 					<figure>
@@ -35,93 +37,35 @@ get_header(); ?>
 	<section class="performers-details">
 		<div class="container">
 			<div class="row">
-				<h1 class="text-center">Take a peek</h1>
+				<h1 class="text-center"><?php echo $performer_details_title; ?></h1>
 				<div class="performers-wrap">
+					<?php
+						$loop = new WP_Query(array('post_type' => 'performer', 'orderby' => 'post_id', 'order' => 'ASC'));
+					?>
+
+					<?php
+					while ($loop->have_posts()) : $loop->the_post() ?>
 					<div class="performer col-sm-4">
 						<div class="col-sm-12">
 							<figure>
-								<img class="img-responsive" src="http://placehold.it/800x400" alt="performer">
+								<?php $image = get_field('performer_image'); ?>
+								<img class="img-responsive" src="<?php echo $image['url']; ?>" alt="performer">
 							</figure>
 						</div>
 						<div class="col-sm-12">
 							<div class="performer-data">
-								<h4>Name: Random Name</h4>
-								<h4>DOB: DD/MM/YY</h4>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident totam repellendus quidem, sed harum deleniti autem! Exercitationem, deserunt. Aliquid sint corporis placeat nihil, eos eveniet, facere incidunt! In odit eaque aliquam nostrum, maxime, molestias ullam pariatur aliquid nulla itaque officiis.</p>
+								<h4>Name: <?php the_field('performer_name'); ?></h4>
+								<h4>DOB: <?php the_field('performer_dob'); ?></h4>
+								<p><?php the_field('performer_introduction'); ?></p>
 							</div>
 						</div>
 					</div>
-					<div class="performer col-sm-4">
-						<div class="col-sm-12">
-							<figure>
-								<img class="img-responsive" src="http://placehold.it/800x400" alt="performer">
-							</figure>
-						</div>
-						<div class="col-sm-12">
-							<div class="performer-data">
-								<h4>Name: Random Name</h4>
-								<h4>DOB: DD/MM/YY</h4>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident totam repellendus quidem, sed harum deleniti autem! Exercitationem, deserunt. Aliquid sint corporis placeat nihil, eos eveniet, facere incidunt! In odit eaque aliquam nostrum, maxime, molestias ullam pariatur aliquid nulla itaque officiis.</p>
-							</div>
-						</div>
-					</div>
-					<div class="performer col-sm-4">
-						<div class="col-sm-12">
-							<figure>
-								<img class="img-responsive" src="http://placehold.it/800x400" alt="performer">
-							</figure>
-						</div>
-						<div class="col-sm-12">
-							<div class="performer-data">
-								<h4>Name: Random Name</h4>
-								<h4>DOB: DD/MM/YY</h4>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident totam repellendus quidem, sed harum deleniti autem! Exercitationem, deserunt. Aliquid sint corporis placeat nihil, eos eveniet, facere incidunt! In odit eaque aliquam nostrum, maxime, molestias ullam pariatur aliquid nulla itaque officiis.</p>
-							</div>
-						</div>
-					</div>
-					<div class="performer col-sm-4">
-						<div class="col-sm-12">
-							<figure>
-								<img class="img-responsive" src="http://placehold.it/800x400" alt="performer">
-							</figure>
-						</div>
-						<div class="col-sm-12">
-							<div class="performer-data">
-								<h4>Name: Random Name</h4>
-								<h4>DOB: DD/MM/YY</h4>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident totam repellendus quidem, sed harum deleniti autem! Exercitationem, deserunt. Aliquid sint corporis placeat nihil, eos eveniet, facere incidunt! In odit eaque aliquam nostrum, maxime, molestias ullam pariatur aliquid nulla itaque officiis.</p>
-							</div>
-						</div>
-					</div>
-					<div class="performer col-sm-4">
-						<div class="col-sm-12">
-							<figure>
-								<img class="img-responsive" src="http://placehold.it/800x400" alt="performer">
-							</figure>
-						</div>
-						<div class="col-sm-12">
-							<div class="performer-data">
-								<h4>Name: Random Name</h4>
-								<h4>DOB: DD/MM/YY</h4>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident totam repellendus quidem, sed harum deleniti autem! Exercitationem, deserunt. Aliquid sint corporis placeat nihil, eos eveniet, facere incidunt! In odit eaque aliquam nostrum, maxime, molestias ullam pariatur aliquid nulla itaque officiis.</p>
-							</div>
-						</div>
-					</div>
-					<div class="performer col-sm-4">
-						<div class="col-sm-12">
-							<figure>
-								<img class="img-responsive" src="http://placehold.it/800x400" alt="performer">
-							</figure>
-						</div>
-						<div class="col-sm-12">
-							<div class="performer-data">
-								<h4>Name: Random Name</h4>
-								<h4>DOB: DD/MM/YY</h4>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident totam repellendus quidem, sed harum deleniti autem! Exercitationem, deserunt. Aliquid sint corporis placeat nihil, eos eveniet, facere incidunt! In odit eaque aliquam nostrum, maxime, molestias ullam pariatur aliquid nulla itaque officiis.</p>
-							</div>
-						</div>
-					</div>
+
+					<?php endwhile; ?>
+					
 				</div>
 			</div>
 		</div>
 	</section>
+	
+<?php get_footer(); ?>
