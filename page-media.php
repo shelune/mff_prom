553684008225d408b5baa1d2452020720e19d5b2
Your Name <you@example.com>
2016-05-08 23:18:21 +0300

    dynamic content for images in gallery

--- page-media.php
+++ page-media.php
@@ -3,6 +3,7 @@
     Template Name: Media Page
  */
 
+$photo_gallery_title = get_field('photo_gallery_title');
 get_header(); ?>
 
 <section class="featured" data-type="background" data-speed="3">
@@ -12,7 +13,7 @@ get_header(); ?>
 <section class="media-content">
 	<div class="container">
 		<div class="row">
-			<h1 class="text-center">Photo Gallery</h1>
+			<h1 class="text-center"><?php echo $photo_gallery_title; ?></h1>
 			<div class="media-tags flex flex--center">
 				<a href="">#TagSomething</a>
 				<a href="">#TagSomething</a>
@@ -20,7 +21,18 @@ get_header(); ?>
 				<a href="">#TagSomething</a>
 			</div>
 			<div class="gallery flex">
-				<?php echo photo_gallery(2); ?>
+				<?php
+				$loop = new WP_Query(array('post_type' => 'gallery_image', 'orderby' => 'post_id', 'order' => 'ASC'));
+				?>
+
+				<?php
+				while ($loop->have_posts()) : $loop->the_post() ?>
+				<figure>
+					<?php $image = get_field('image_content'); ?>
+					<img src="<?php echo $image['url'];  ?>" alt="<?php the_field('image_alt'); ?>" class="img-responsive" tag-data="<?php the_field('image_tag') ?>">
+				</figure>
+
+				<?php endwhile; ?>
 
 			</div>
 		</div>
