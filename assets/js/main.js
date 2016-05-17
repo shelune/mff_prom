883529bd430db572b6a3e83ec5a6223076e1129d
Your Name <you@example.com>
2016-05-18 01:40:36 +0300

    content now displayed when in viewport

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
 	var headerHeight = $('.navbar').outerHeight();
 	var landingHeight = $('.jumbotron').outerHeight();
 	
-	console.log(landingHeight);
 
 	// paralax background effect
 	$('section[data-type="background"]').each(function () {
@@ -35,6 +34,52 @@ $(document).ready(function () {
 		} else {
 			target.removeClass('navbar--show');
 		}
+		
+		// change color of scroll arrow (funky AF)
+		if (currentY > 110) {
+			$('.scroll-trigger').addClass('inverse');
+			changeArrDirection("up");
+		} else {
+			$('.scroll-trigger').removeClass('inverse');
+			changeArrDirection("down");
+		}
+		
+		if (currentY > 780) {
+			$('.scroll-trigger').removeClass('inverse');
+		}
+		
+		if (currentY > 1450) {
+			$('.scroll-trigger').addClass('inverse');
+		}
+		
+		if (currentY > 1900) {
+			$('.scroll-trigger').removeClass('inverse');
+		}
+	});
+	
+	// change arrow direction of scroll arrow fixed bottom
+	var changeArrDirection = function (direction) {
+		if (direction === "up") {
+			$('.scroll-trigger i').removeClass('fa-angle-down');
+			$('.scroll-trigger i').addClass('fa-angle-up');
+		} else {
+			$('.scroll-trigger i').removeClass('fa-angle-up');
+			$('.scroll-trigger i').addClass('fa-angle-down');
+		}
+	};
+	
+	$('.intro__last-year, .intro__this-year').addClass('viewport--hidden-left').viewportChecker({
+		classToAddForFullView: 'viewport--show',
+		classToRemove: 'viewport--hidden-left',
+		repeat: true,
+		offset: 250
+	});
+	
+	$('.performer--old').addClass('viewport--hidden-right').viewportChecker({
+		classToAddForFullView: 'viewport--show',
+		classToRemove: 'viewport--hidden-right',
+		repeat: true,
+		offset: 250
 	});
 
 });
